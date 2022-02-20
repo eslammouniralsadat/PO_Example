5e976e6ad7d2fdd8be769a2fabe38a87befc3bf9
Eslam Mounir AL Sadat <alsadateslammounir@hotmail.com>
2022-02-20 13:00:24 +0200

    Add project files.

---
+++ Precedence_Operators_Example/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Variables
+
+int a = 20;
+int b = 10;
+int c = 15;
+int d = 5;
+
+int resul;
+
+// Arithmetic Operations
+
+resul = (a + b) * c / d;    // ==>  ( 30 * 15 ) / 5
+Console.WriteLine("Value of ==> (a + b) * c / d is : {0}", resul); // Output => 90
+
+resul = ((a + b) * c) / d;  // ==>  (30 * 15 ) / 5
+Console.WriteLine("Value of ==> ((a + b) * c) / d is  : {0}", resul); // Output => 90
+
+
